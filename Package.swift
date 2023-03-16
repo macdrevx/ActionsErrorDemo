3f48009e6c57ae7ef6dd42717a264885ada429fc
Andrew Hershberger <andrew.d.hershberger@gmail.com>
2023-03-16 10:16:42 -0400

    Add Swift package to demonstrate GH Actions bug

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "ActionsErrorDemo",
+    products: [
+        .library(name: "ActionsErrorDemo", targets: ["ActionsErrorDemo"]),
+    ],
+    targets: [
+        .target(name: "ActionsErrorDemo"),
+        .testTarget(
+            name: "ActionsErrorDemoTests",
+            dependencies: ["ActionsErrorDemo"],
+            resources: [
+                .copy("image.png")
+            ]),
+    ]
+)
---
+++ Tests/ActionsErrorDemoTests/ActionsErrorDemoTests.swift
@@ -0,0 +1,13 @@
+@testable import ActionsErrorDemo
+
+import XCTest
+
+final class ActionsErrorDemoTests: XCTestCase {
+	func testResourceAccess() {
+		if let url = Bundle.module.url(forResource: "image", withExtension: "png") {
+			print(url)	
+		} else {
+			print("didn't find image.png")
+		}
+	}
+}
